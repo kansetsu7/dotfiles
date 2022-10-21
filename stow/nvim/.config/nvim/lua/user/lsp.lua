83e476c816c247d0e16a6b8e7b14231129aa8f73
Andre Kuo <andre.kuo@abagile.com>
2022-10-21 11:50:49 +0800

    Update cmp_nvim_lsp config
    update_capabilities is deprecated, use default_capabilities instead.

--- stow/nvim/.config/nvim/lua/user/lsp.lua
+++ stow/nvim/.config/nvim/lua/user/lsp.lua
@@ -75,7 +75,7 @@ local on_attach = function(client, bufnr)
   lsp_highlight_document(client)
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local servers = {
   "sumneko_lua",
