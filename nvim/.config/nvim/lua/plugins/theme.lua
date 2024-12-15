23e88954a060c3a3ec6aa66b9f86f3fa203d92c3
Andre Kuo <andre.kuo@abagile.com>
2024-12-15 21:21:29 +0800

    Update theme
    Nord is not my type, and dracula somehow doesn't looks well after
    switch to WC's configs.
    Thus tried kanagawa, which I want to try years ago.
    
    Just a note for my later self: don't use kanagawa-dragon, contrast is
    too low.

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,55 +1,20 @@
 return {
   {
-    "shaunsingh/nord.nvim",
+   "shaunsingh/nord.nvim",
+   config = function()
+     -- vim.cmd [[colorscheme nord]]
+   end
+  },
+  {
+    'dracula/vim',
     config = function()
-      vim.cmd [[colorscheme nord]]
+      -- vim.cmd [[colorscheme dracula]]
     end
   },
   {
-    "Mofiqul/vscode.nvim",
-    branch = "main",
+    'rebelot/kanagawa.nvim',
     config = function()
-      -- vim.g.vscode_style = "dark"
-      -- vim.cmd [[colorscheme vscode]]
+      vim.cmd [[colorscheme kanagawa]]
     end
   },
-  {
-    "navarasu/onedark.nvim",
-    config = function()
-      -- vim.g.onedark_config = { style = "warmer" }
-      -- vim.cmd [[colorscheme onedark]]
-    end
-  },
-  {
-    "folke/tokyonight.nvim",
-    config = function()
-      -- require("tokyonight").setup({
-      -- style = "night",
-      -- })
-      -- vim.cmd [[colorscheme tokyonight]]
-    end
-  },
-  {
-    "marko-cerovac/material.nvim",
-    branch = "main",
-    config = function()
-      -- vim.g.material_style = "darker"
-      -- vim.cmd [[colorscheme material]]
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-      -- vim.cmd([[colorscheme catppuccin]])
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function ()
-      -- vim.o.background = "dark" -- or "light" for light mode
-      -- vim.cmd([[colorscheme gruvbox]])
-    end
-  }
 }
