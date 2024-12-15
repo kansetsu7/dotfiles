b438713da4bc437ae4ed5b93b6f43b5229beccd0
Andre Kuo <andre.kuo@abagile.com>
2024-12-15 21:05:15 +0800

    Disable buffer icons

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -95,7 +95,7 @@ return {
           --   end
           -- end,
           offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
-          show_buffer_icons = true,
+          show_buffer_icons = false,
           show_buffer_close_icons = false,
           show_close_icon = false,
           show_tab_indicators = true,
