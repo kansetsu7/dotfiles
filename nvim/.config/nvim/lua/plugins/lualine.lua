6197102be009f8faa023a506b67e405562a800dd
Andre Kuo <andre.kuo@abagile.com>
2025-07-03 09:55:05 +0800

    Update lualine filename display logic
    Was only display filename but sometimes don't have enough info.
    Use path = 4 to display parent folder as well.
    
    Ref:
    https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#filename-component-options

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return {
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = { 'filename' },
+          lualine_c = { { 'filename', path = 4 } },
           lualine_x = { 'location' },
           lualine_y = {},
           lualine_z = {}
@@ -17,7 +17,7 @@ return {
         sections = {
           lualine_a = { 'mode' },
           lualine_b = { 'diff', 'diagnostics' },
-          lualine_c = { 'filename' },
+          lualine_c = { { 'filename', path = 4 } },
           lualine_x = { 'encoding', 'fileformat', 'filetype' },
           lualine_y = { 'progress' },
           lualine_z = { 'location' }
